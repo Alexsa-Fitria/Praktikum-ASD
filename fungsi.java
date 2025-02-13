0ce1506643239183cc1e58e3664c135023f83aae
alexsa <lexafitriayu@gmail.com>
2025-02-13 10:52:21 +0700

    Add files via upload

---
+++ fungsi.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+
+public class fungsi {
+    static int[][] stokBunga;
+    static int[] hargaBunga = {75000, 50000, 60000, 10000};
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        stokBunga = new int[4][4];  
+
+        for (int i = 0; i < stokBunga.length; i++) {
+            for (int j = 0; j < stokBunga[i].length; j++) {
+                System.out.print("Masukkan stok bunga " + namaBunga(j) + " untuk cabang " + (i + 1) + ": ");
+                stokBunga[i][j] = sc.nextInt();
+            }
+        }
+
+        tampilkanPendapatanCabang();
+        tampilkanJumlahStokBunga();
+        perbaruiStokBunga(sc);
+        tampilkanJumlahStokBunga();  
+
+        sc.close();
+    }
+
+    public static void tampilkanPendapatanCabang() {
+        for (int i = 0; i < stokBunga.length; i++) {
+            int pendapatan = 0;
+            for (int j = 0; j < stokBunga[i].length; j++) {
+                pendapatan += stokBunga[i][j] * hargaBunga[j];
+            }
+            System.out.println("Pendapatan RoyalGarden " + (i + 1) + ": " + pendapatan);
+        }
+    }
+
+    public static void tampilkanJumlahStokBunga() {
+        int[] totalStok = new int[stokBunga[0].length];
+        for (int j = 0; j < stokBunga[0].length; j++) {
+            int jumlah = 0;
+            for (int i = 0; i < stokBunga.length; i++) {
+                jumlah += stokBunga[i][j];
+            }
+            totalStok[j] = jumlah;
+            System.out.println("Jumlah stok " + namaBunga(j) + ": " + jumlah);
+        }
+    }
+
+    public static void perbaruiStokBunga(Scanner input) {
+        System.out.println("Masukkan pengurangan stok bunga mati untuk setiap cabang dan setiap jenis bunga:");
+
+        for (int i = 0; i < stokBunga.length; i++) {
+            for (int j = 0; j < stokBunga[i].length; j++) {
+                System.out.print("Pengurangan stok bunga " + namaBunga(j) + " untuk cabang " + (i + 1) + ": ");
+                int penguranganStok = input.nextInt();
+                stokBunga[i][j] -= penguranganStok;
+            }
+        }
+
+        System.out.println("Stok bunga telah diperbarui berdasarkan informasi pengurangan stok.");
+    }
+
+    public static String namaBunga(int index) {
+        String[] namaBunga = {"Aglonema", "Keladi", "Alocasia", "Mawar"};
+        return namaBunga[index];
+    }
+}
